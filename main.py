92f7dc66b412b819b5990903689fd9d555576f6b
JaimeCabreraDev <jacabrer@bancolombia.com.co>
2022-11-06 13:52:04 -0500

    Models vet and CRUD owners

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.openapi.utils import get_openapi
+
+from router.app_routers import app_router
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Vet App",
+        version="1.0.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+app.openapi = custom_openapi
 
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+app.include_router(app_router)
